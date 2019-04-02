6b84eaf1684618a4593863ffe790a9c93e22ba36
gangrong.deng <gangrong.deng@ucarinc.com>
2019-04-02 16:08:22 +0800

    first commit

---
+++ page/component/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '/image/sy1.jpg',
+      '/image/sy2.jpg',
+      '/image/sy3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
